a8272e91c16d509f37404353e9eb0162b2e51d2e
Dan Conrad <dan.m.conrad@gmail.com>
2019-03-11 22:28:06 -0500

    Uses HashHistory because of github

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Home from '../Home';
 import Dashboard from '../Dashboard';
@@ -8,11 +8,11 @@ import Footer from '../Footer';
 import './index.css';
 
 export default () => (
-  <Router>
+  <HashRouter>
     <div className="App">
       <Route exact path="/" component={Home} />
       <Route path="/dashboard" component={Dashboard} />
       <Footer />
     </div>
-  </Router>
+  </HashRouter>
 );
