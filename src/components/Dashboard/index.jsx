8548f7e69dca5e09c681e220d4a45c4d71f9d0d5
Dan Conrad <dan.m.conrad@gmail.com>
2019-03-17 17:19:15 -0500

    Periodically refreshes

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -5,18 +5,19 @@ import Character from '../Character';
 import fromBeyond from '../Character/fromBeyond';
 import styles from './index.scss';
 
-// const ONE_SECOND = 1000;
+const ONE_SECOND = 1000;
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 class Dashboard extends Component {
   state = {
     characterConfig: null,
+    characterIDs: [],
     characters: [],
-    refreshIdx: 0,
     params: {},
   }
 
   componentWillMount() {
-    this.refreshNext = this.refreshNext.bind(this);
+    this.getCharacters = this.getCharacters.bind(this);
 
     this.setState({
       characterIDs: (this.props.match.params.characterIDs || '').split(',')
@@ -29,39 +30,22 @@ class Dashboard extends Component {
     api.characterConfig()
       .then(characterConfig => this.setState({ characterConfig }))
       .then(() => this.getCharacters());
+
+    !IS_DEV && setInterval(this.getCharacters, 60 * ONE_SECOND);
   }
 
   componentWillUnmount() {
-    // !config.isDev && clearInterval(this.refreshNext);
+    !IS_DEV && clearInterval(this.refreshNext);
   }
 
   getCharacters() {
     api.characters(this.state.characterIDs)
       .then(characters => characters.map(c => this.transformCharacter(c)))
       .then(characters => this.setState({ characters }));
-
-    // !config.isDev && setInterval(this.refreshNext, 30 * ONE_SECOND);
   }
 
   transformCharacter(character) {
     return fromBeyond(this.state.characterConfig, character);
-  }
-
-  refreshNext() {
-    const {characters} = this.state;
-
-    api.character(this.state.characterIDs[this.state.refreshIdx])
-      .then(character => characters[this.state.refreshIdx] = fromBeyond(character))
-      .then(() => this.setState({ characters }))
-      .then(() => this.incrementIndex());
-  }
-
-  incrementIndex() {
-    let newIndex = this.state.refreshIdx + 1;
-
-    this.setState({
-      refreshIdx: newIndex >= this.state.characters.length ? 0 : newIndex
-    });
   }
 
   render() {
